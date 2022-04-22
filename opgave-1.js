c62e1d6922aa8a6bcceba844632e0cf193935f23
KlausP1 <112825@emmauscollege.nl>
2022-04-22 07:50:08 +0000

    opgave 1, 2 & 3 gemaakt

--- opgave-1.js
+++ opgave-1.js
@@ -11,6 +11,11 @@ function isPositief(getal) {
   var teruggeefWaarde = undefined;
 
   // vul hier aan:
+  if(getal >= 0){
+    teruggeefWaarde = true
+  } else {
+    teruggeefWaarde = false
+  }
   
 
   return teruggeefWaarde;
